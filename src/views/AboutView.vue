c5993365f148c722a37d2cfe8bb4512fefcbad56
HongDaeEui <day100@enterblock.co>
2023-01-20 09:33:29 +0900

    init

---
+++ src/views/AboutView.vue
@@ -0,0 +1,44 @@
+<script setup>
+const search = ref("")
+const names = ref(['aa', 'bb', 'cc', 'dd'])
+
+const matchingNames = computed(() => {
+    return names.value.filter(name => name.includes(search.value))
+})
+
+watch(search, () => {
+    "search 값이 바뀔 때 마다 실행되는 함수";
+})
+
+watchEffect(() => {
+    console.log(
+        "search value가 정의됬기에 search가 바뀔때마다 실행된다",
+        search.value,
+    )
+})
+
+
+</script>
+
+
+<template>
+    <div class="about">
+
+        <h1>unplugin-auto-import 사용</h1>
+        <input type="text" v-model="search"/>
+        <p>{{ search }}</p>
+        <div v-for="name in matchingNames" :key="name">
+            {{ name }}
+        </div>
+    </div>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+    .about {
+        min-height: 100vh;
+        display: flex;
+        align-items: center;
+    }
+}
+</style>
---
+++ src/views/Pinia.vue
@@ -0,0 +1,35 @@
+<script setup>
+import {computed, ref} from "vue";
+import {useListStore} from "@/stores/list";
+
+const text = ref("")
+const list = useListStore()
+
+const addList = () => {
+    if (!text.value) return
+    list.addList(text.value)
+    text.value = ""
+}
+
+const getDataAll = computed(() =>
+    list.getDataAll)
+
+for (const [index, value] of [1, 2, 3, 4, 5].entries()) {
+    console.log(index, value);
+}
+
+
+
+</script>
+
+
+<template>
+    <div>
+        <input type="text" v-model="text"/>
+        <button @click="addList(text)">추가</button>
+        <h4>추가된 목록</h4>
+        <p v-for="(item, index) in getDataAll" :key="index">
+            {{ item }}
+        </p>
+    </div>
+</template>
